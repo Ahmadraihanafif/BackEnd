8308ce3af89d78dac509d341c76ba09b348a32a3
Ahmadraihanafif <afif.asikin@gmail.com>
2022-11-03 21:31:42 +0700

    Task Pertemuan 5

--- restful_api/routes/api.php
+++ restful_api/routes/api.php
@@ -25,8 +25,6 @@ Route::post('/animals', [AnimalController::class, 'store']);
 Route::put('/animals/{id}', [AnimalController::class, 'update']);
 Route::delete('/animals/{id}', [AnimalController::class, 'destroy']);
 
-
 Route::get('/students', [StudentController::class, 'index']);
-Route::post('/students', [StudentController::class, 'store']);
-Route::put('/students/{id}', [StudentController::class, 'update']);
-Route::delete('/students/{id}', [StudentController::class, 'destroy']);
+
+Route::posh('/students', [StudentController::class, 'index']);
